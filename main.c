dd657bba4ae928316e034b274d82c8b195b1384b
Mickael Chen <micchen@c2r6p1.42lehavre.fr>
2025-03-27 10:58:38 +0100

    small fixes

--- main.c
+++ main.c
@@ -12,10 +12,37 @@
 
 #include "cube3d.h"
 
+int	check_arg(char *argv, char *ext)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (argv[i] != '.')
+		i++;
+	j = i;
+	if (ft_strlen(argv) - i != 4)
+		return (ft_printf(2, "Error: wrong file1\n"), 0);
+	j = 0;
+	while (argv[i])
+	{
+		if (argv[i] == ext[j])
+		{
+			i++;
+			j++;
+		}
+		else
+			return (ft_printf(2, "Error: wrong file2\n"), 0);
+	}
+	printf("ouais\n");
+	return (1);
+}
+
 int	main(int argc, char **argv)
 {
 	t_game	game;
-
+	if (argc != 2 || check_arg(argv[1], ".cub"))
+	{
 	if (parsing(&game, argc, argv) == ERROR)
 		return (clean_game(&game), ERROR);
 	game.session = mlx_init();
@@ -31,4 +58,5 @@ int	main(int argc, char **argv)
 	mlx_key_hook(game.window, keycode_value, &game);
 	mlx_loop(game.session);
 	return (close_window(&game), 0);
+	}
 }
